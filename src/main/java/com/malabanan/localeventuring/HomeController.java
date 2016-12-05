f8e5f67de1f2cd5c1e75a28a777e2d42d546c515
Milliza Malabanan <milliza.mae@gmail.com>
2016-12-05 15:50:08 -0500

    mapping annotations and corresponding jsp pages

--- src/main/java/com/malabanan/localeventuring/HomeController.java
+++ src/main/java/com/malabanan/localeventuring/HomeController.java
@@ -4,6 +4,8 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -25,15 +27,70 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
 		
-		Contact contact = new Contact();
+		Venue venue = new Venue();
 		
-		contact.setName("Frank");
-		contact.setAddress("Test");
-		contact.setEmail("email");
+		venue.setContactId(222);
+		venue.setVenueName("GC");
+		venue.setRoomSize(3242);
+		venue.setCapacity(3);
+		venue.setPrice(342);
+		venue.setPhotoLink("43");
+		venue.setCalendarLink("dfadf");
+		venue.setDescription("4ksdjf;a");
 		
-		DAOContact.addContact(contact);
+		DAOVenue.addVenue(venue);
 		
 		return "home";
 	}
+	@RequestMapping(value = "/addform", method = RequestMethod.GET)
+	public String addform(Model model, HttpServletRequest request) {
+		
+		model.addAttribute("venuename", request.getParameter("venuename"));
+		model.addAttribute("roomsize", request.getParameter("roomsize"));
+		model.addAttribute("capacity", request.getParameter("capacity"));
+		model.addAttribute("price", request.getParameter("price"));
+		model.addAttribute("photolink", request.getParameter("photolink"));
+		model.addAttribute("calendarlink", request.getParameter("calendarlink"));
+		model.addAttribute("description", request.getParameter("description"));
+		
+		Venue venue = new Venue(); 
+		
+		// venue.setContactId(222);
+		venue.setVenueName(request.getParameter("venuename"));
+		venue.setRoomSize(Integer.parseInt(request.getParameter("roomsize")));
+		venue.setCapacity(Integer.parseInt(request.getParameter("capacity")));
+		venue.setPrice(Integer.parseInt(request.getParameter("price")));
+		venue.setPhotoLink(request.getParameter("photolink"));
+		venue.setCalendarLink(request.getParameter("calendarlink"));
+		venue.setDescription(request.getParameter("description"));
+		
+		DAOVenue.addVenue(venue);
+		
+		return "addresults";
+	}
 	
+	@RequestMapping(value = "/addcontact", method = RequestMethod.GET)
+	public String addcontact(Model model, HttpServletRequest request) {
+		
+		model.addAttribute("firstname", request.getParameter("firstname"));
+		model.addAttribute("lastname", request.getParameter("lastname"));
+		model.addAttribute("email", request.getParameter("email"));
+		model.addAttribute("street", request.getParameter("street"));
+		model.addAttribute("city", request.getParameter("city"));
+		model.addAttribute("state", request.getParameter("state"));
+		model.addAttribute("zipcode", request.getParameter("zipcode"));
+		
+		Contact contact = new Contact(); 
+		
+		// venue.setContactId(222);
+		contact.setName(request.getParameter("firstname") + request.getParameter("lastname"));
+		contact.setEmail(request.getParameter("email"));
+		contact.setAddress(request.getParameter("street") + request.getParameter("city") 
+			+ request.getParameter("state") + request.getParameter("zipcode"));
+
+		
+		DAOContact.addContact(contact);
+		
+		return "contactsummary";
+}
 }
---
+++ src/main/webapp/WEB-INF/views/addresults.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Venue Added!</title>
+</head>
+<h1>Venue Added</h1>
+<body>
+
+${venuename }<br>
+${roomsize }<br>
+${capacity }<br>
+${price }<br>
+${photolink }<br>
+${calendarlink }<br>
+${description }<br> 
+</body>
+</html>
