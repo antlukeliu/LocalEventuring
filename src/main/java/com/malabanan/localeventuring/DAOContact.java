0c655b6d393507668f0a2a011312b1f601ab5132
AntLukeLiu <antmliu@gmail.com>
2016-12-05 11:33:50 -0500

    Created the DAO

---
+++ src/main/java/com/malabanan/localeventuring/DAOContact.java
@@ -0,0 +1,52 @@
+package com.malabanan.localeventuring;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class DAOContact {
+
+	private static SessionFactory factory;
+
+	private static void setupFactory() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (Exception e) {
+			;
+		}
+		Configuration configuration = new Configuration();
+		// modify these to match your XML files
+		configuration.configure("hibernate.cfg.xml");
+		configuration.addResource("contact.hbu.xml");
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+				.applySettings(configuration.getProperties()).build();
+		factory = configuration.buildSessionFactory(serviceRegistry);
+	}
+	
+	public static int addContact(Contact c) {
+		if (factory == null)
+			setupFactory();
+		Session hibernateSession = factory.openSession();
+		hibernateSession.getTransaction().begin();
+		// save this specific record
+		int i = (Integer) hibernateSession.save(c);
+		hibernateSession.getTransaction().commit();
+		hibernateSession.close();
+		return i;
+	}
+	
+	public static List<Contact> getContact(String str) {
+		if (factory == null)
+			setupFactory();
+		Session hibernateSession = factory.openSession();
+		hibernateSession.getTransaction().begin();
+		List<Contact> contact = hibernateSession.createQuery(str).list();
+		hibernateSession.getTransaction().commit();
+		hibernateSession.close();
+		return contact;
+	}
+}
