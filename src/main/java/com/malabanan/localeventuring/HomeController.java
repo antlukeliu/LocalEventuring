923ee2b3b82fdb0d74b28f8de6f00c01aefa4c6c
Frank Daidone <daidonef@msu.edu>
2016-12-08 18:14:32 -0500

    testing

--- src/main/java/com/malabanan/localeventuring/HomeController.java
+++ src/main/java/com/malabanan/localeventuring/HomeController.java
@@ -69,7 +69,7 @@ public class HomeController {
 		}
 		String capacity = "";
 		if (request.getParameter("people") != null) {
-			capacity = request.getParameter("people");
+			capacity = request.getParameter("people"); 
 		}
 
 		String[] filters = { price, capacity };
